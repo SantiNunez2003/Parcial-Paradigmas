b07052e6fcf924c9a15f07a8cd28381508698d1d
Santiago Nuñez <133709203+SantiNunez2003@users.noreply.github.com>
2024-10-30 16:05:25 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const votarButtons = document.querySelectorAll(".votar");
-
-votarButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        alert("¡Gracias por tu voto!");
-        // Aquí puedes agregar la lógica para enviar el voto al servidor utilizando AJAX.
-    });
-});
